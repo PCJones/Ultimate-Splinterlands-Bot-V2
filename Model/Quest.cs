d01ba41e144b80cfc76a318a7800d83638755fa9
Jonas Friedrich <pcjones@live.de>
2022-06-02 18:03:19 +0200

    Switch from .net 5.0 to 6.0

---
+++ Model/Quest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultimate_Splinterlands_Bot_V2.Model
+{
+    internal class Quest
+    {
+    }
+}
